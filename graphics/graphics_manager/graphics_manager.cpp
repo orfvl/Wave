403d12da8aabe2b0659245fbc9723a4d3e4ad321
orfvl <oscarvonloehneysen@gmail.com>
2024-02-20 10:44:34 +0100

    refraction of code

--- graphics/graphics_manager/graphics_manager.cpp
+++ graphics/graphics_manager/graphics_manager.cpp
@@ -2,7 +2,7 @@
 
 
 GraphicsManager::GraphicsManager(int width_, int height_, const char* title_)
-width(width_), height(height_), title(title_) {}
+:   width{width_}, height{height_}, title{title_} {};
 
 void GraphicsManager::init()
 {
@@ -17,13 +17,13 @@ void GraphicsManager::init()
 
     glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
 
-     GLFWwindow* window = glfwCreateWindow(SCR_width, SCR_height, "Wave animation", nullptr, nullptr); // Window
+    window = glfwCreateWindow(SCR_width, SCR_height, "Wave animation", nullptr, nullptr); // Window
     //GLFWwindow* window2 = glfwCreateWindow(800, 600, "OpenGL", glfwGetPrimaryMonitor(), nullptr); // Fullscreen
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
-        return -1;
+        // return -1;
     }
     glfwMakeContextCurrent(window);
     framebuffer_size_callback(window, SCR_width, SCR_height);
@@ -36,24 +36,23 @@ void GraphicsManager::init()
 
       // build and compile our shader program
     // ------------------------------------
-    Shader ourShader = ResourceManager::LoadShader("shader.vs", "shader.fs", nullptr, "wave");
+    
      ourShader.Use();
 
    
     
-    TextRenderer  *Text;
     Text = new TextRenderer(1000, 1000);
     Text->Load("a_ReportSansRgh.ttf", 24);
 
 
-    unsigned int VBO, VAO;
+    
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices.begin(), GL_STATIC_DRAW);
 
     // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -66,6 +65,57 @@ void GraphicsManager::init()
     ourShader.SetVector4f("ourColor", {0.0f, 0.8f, 1.0f, 1.0f}, true);
 
     
+}
+
+void GraphicsManager::framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+void GraphicsManager::processInput()
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+    {
+        distanceCamera += 1.0f; // change this value accordingly (might be too slow or too fast based on system hardware)
+        
+    }
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+    {
+        distanceCamera -= 1.0f; // change this value accordingly (might be too slow or too fast based on system hardware)
+    }
+     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
+        angleRotateXachsis -= 1.0f; // change this value accordingly (might be too slow or too fast based on system hardware)
+        
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
+        angleRotateXachsis += 1.0f; // change this value accordingly (might be too slow or too fast based on system hardware)
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    {
+        angleRotateYachsis -= 1.0f; // change this value accordingly (might be too slow or too fast based on system hardware)
+        
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    {
+        angleRotateYachsis += 1.0f; // change this value accordingly (might be too slow or too fast based on system hardware)
+    }
+    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+    {
+        gameTimeFactor /= 1.1f; // change this value accordingly (might be too slow or too fast based on system hardware)
+        
+    }
+    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+    {
+        gameTimeFactor *= 1.1f; // change this value accordingly (might be too slow or too fast based on system hardware)
+    }
+
 }
 
 
--- graphics/graphics_manager/graphics_manager.hpp
+++ graphics/graphics_manager/graphics_manager.hpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <array>
+#include <sstream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
@@ -14,19 +17,20 @@ public:
     GraphicsManager(int width, int height, const char* title);
     ~GraphicsManager();
     void init();
-    void run();
+    void processInput();
+    template<typename T>
+    void render(T  moved_points);
 
 private:
     int SCR_width = 1000;
     int SCR_height = 800;
     GLFWwindow* window;
     int width, height;
-    const char*
-}
+    const char* title;
 
-// set up vertex data (and buffer(s)) and configure vertex attributes
+    // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
-    float vertices[] = {
+    std::array<float,6*6*3> const vertices = {
         -0.5f, -0.5f, -0.5f,  
          0.5f, -0.5f, -0.5f,  
          0.5f,  0.5f, -0.5f,  
@@ -69,5 +73,67 @@ private:
         -0.5f,  0.5f,  0.5f,  
         -0.5f,  0.5f, -0.5f
     };
+    unsigned int VBO, VAO;
+    
+    Shader ourShader = ResourceManager::LoadShader("shader.vs", "shader.fs", nullptr, "wave");
+    TextRenderer  *Text;
+    
+    float distanceCamera = 45.0;
+    float angleRotateXachsis = 0.0;
+    double gameTimeFactor = 1;
+    float angleRotateYachsis = 0.0;
 
    
+    void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+};
+
+template<typename T>
+    // requires Regular<T>
+void GraphicsManager::render( T moved_points)
+{
+  
+    // Render outputs
+       // render
+        // ------
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // also clear the depth buffer now!
+        std::stringstream ss;
+        ss << gameTimeFactor;
+        Text->RenderText("gameTimeFactor: "+ss.str(), 5.0f, 5.0f, 1.0f);
+
+        // activate shader
+        ourShader.Use();
+
+
+        // create transformations
+        glm::mat4 view          = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+        glm::mat4 projection    = glm::mat4(1.0f);
+        projection = glm::perspective(glm::radians(distanceCamera), (float)SCR_width / (float)SCR_height, 0.1f, 100.0f);
+        view       = glm::translate(view, glm::vec3(0.0f, 0.0f, -50.0f));
+        view       = glm::rotate(view, glm::radians(angleRotateXachsis) , glm::vec3(1.0f , 0.0f, 0.0f));
+        view       = glm::rotate(view, glm::radians(angleRotateYachsis) , glm::vec3(0.0f , 1.0f, 0.0f));
+        // pass transformation matrices to the shader
+        ourShader.SetMatrix4("projection", projection); // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
+        ourShader.SetMatrix4("view", view);
+
+        // render boxes
+        glBindVertexArray(VAO);
+        for (auto i : moved_points)
+        {
+            // calculate the model matrix for each object and pass it to shader before drawing
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model,glm::vec3(i.x,i.y, i.z) );
+            ourShader.SetMatrix4("model", model);
+            ourShader.SetVector4f("ourColor", {std::abs(i.d), 0.5f, 0.5f, 1.0f}, false);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+
+
+
+         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+}
+#endif
